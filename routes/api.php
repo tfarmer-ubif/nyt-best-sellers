38b7e6f4020964510f45ff990b3c07dfd64a21e2
tfarmer-ubif <timothy.farmer@asurion.com>
2024-11-12 19:43:04 -0600

    adding ApiController to route file

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ApiController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/1/nyt/best-sellers', [ApiController::class, 'bestSellers']);
